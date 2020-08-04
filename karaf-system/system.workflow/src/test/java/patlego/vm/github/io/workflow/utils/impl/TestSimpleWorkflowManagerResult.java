762760de7d7fe2e355f1a25960576be5d3918895
Patrique Legault <patrique.legault@gmail.com>
2020-08-04 14:06:51 -0400

    Adding conversion test

--- karaf-system/system.workflow/src/test/java/patlego/vm/github/io/workflow/utils/impl/TestSimpleWorkflowManagerResult.java
+++ karaf-system/system.workflow/src/test/java/patlego/vm/github/io/workflow/utils/impl/TestSimpleWorkflowManagerResult.java
@@ -5,20 +5,60 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
+import patlego.vm.github.io.datasource.workflowmanager.tables.WorkItemId;
 import patlego.vm.github.io.datasource.workflowmanager.tables.WorkflowManagerWF;
+import patlego.vm.github.io.datasource.workflowmanager.tables.WorkflowManagerWI;
 import patlego.vm.github.io.workflow.utils.WorkflowManagerResult;
 
 public class TestSimpleWorkflowManagerResult {
-    
+
+    String randString;
+
+    WorkflowManagerWF manager = new WorkflowManagerWF();
+
+    WorkItemId workItemId = new WorkItemId();
+
+    WorkflowManagerWI workItem1 = new WorkflowManagerWI();
+    WorkflowManagerWI workItem2 = new WorkflowManagerWI();
+    WorkflowManagerWI workItem3 = new WorkflowManagerWI();
+    WorkflowManagerWI workItem4 = new WorkflowManagerWI();
+
+    List<WorkflowManagerWI> workItems = new LinkedList<WorkflowManagerWI>();
+
+    @BeforeEach
+    public void setup() {
+        this.randString = UUID.randomUUID().toString();
+        
+        this.manager.setWorkflowId(randString);
+
+        this.workItemId.setWorkflowId(this.manager);
+        this.workItemId.setWorkitemName(randString);
+
+        this.workItem1.setId(this.workItemId);
+        this.workItem2.setId(this.workItemId);
+        this.workItem3.setId(this.workItemId);
+        this.workItem4.setId(this.workItemId);
+
+        this.workItems.add(workItem1);
+        this.workItems.add(workItem2);
+        this.workItems.add(workItem3);
+        this.workItems.add(workItem4);
+    }
+
     @Test
     public void convertSimpleWorkflowManagerResult() {
         String id = UUID.randomUUID().toString();
         LocalDateTime time = LocalDateTime.now();
-        
+
         WorkflowManagerWF entity = new WorkflowManagerWF();
         entity.setWorkflowId(id);
         entity.setEndTime(Timestamp.valueOf(time));
@@ -39,7 +79,7 @@ public class TestSimpleWorkflowManagerResult {
     public void convertWorkflowManagerWF() {
         String id = UUID.randomUUID().toString();
         LocalDateTime time = LocalDateTime.now();
-        
+
         SimpleWorkflowManagerResult entity = new SimpleWorkflowManagerResult(id);
         entity.addEndTime(time);
         entity.addStartTime(time);
@@ -57,9 +97,40 @@ public class TestSimpleWorkflowManagerResult {
     }
 
     @Test
+    public void convertSimpleWorkflowManagerResult_withList() {
+        LocalDateTime time = LocalDateTime.now();
+
+        WorkflowManagerWF entity = Mockito.mock(WorkflowManagerWF.class);
+        Mockito.doNothing().when(entity).setWorkflowId(Mockito.anyString());
+        Mockito.doNothing().when(entity).setEndTime(Mockito.any(Timestamp.class));
+        Mockito.doNothing().when(entity).setStartTime(Mockito.any(Timestamp.class));
+        Mockito.doNothing().when(entity).setSuccess(Mockito.anyBoolean());
+
+        Mockito.when(entity.getWorkItems()).thenReturn(this.workItems);
+        Mockito.when(entity.getWorkflowId()).thenReturn(this.randString);
+        Mockito.when(entity.getEndTime()).thenReturn(Timestamp.valueOf(time));
+        Mockito.when(entity.getStartTime()).thenReturn(Timestamp.valueOf(time));
+        Mockito.when(entity.getSuccess()).thenReturn(Boolean.TRUE);
+        
+        entity.setWorkflowId(this.randString);
+        entity.setEndTime(Timestamp.valueOf(time));
+        entity.setStartTime(Timestamp.valueOf(time));
+        entity.setSuccess(true);
+
+        WorkflowManagerResult result = SimpleWorkflowManagerResult.create(entity);
+
+        assertEquals(this.randString, result.getId());
+        assertEquals(time, result.getEndTime());
+        assertEquals(time, result.getStartTime());
+        assertEquals(true, result.getWorkflowSucceddedStatus());
+
+        assertEquals(4, result.getWorkItems().size());
+    }
+
+    @Test
     public void convertSimpleWorkflowManagerResult_InvalidID() {
         WorkflowManagerWF entity = new WorkflowManagerWF();
-        assertThrows(IllegalArgumentException.class, () ->  {
+        assertThrows(IllegalArgumentException.class, () -> {
             SimpleWorkflowManagerResult.create(entity);
         });
     }
