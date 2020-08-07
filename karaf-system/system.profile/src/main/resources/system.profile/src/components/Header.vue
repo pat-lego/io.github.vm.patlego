c3b20e23bb7c0151ba30032a07022858b4c26bc7
Patrique Legault <patrique.legault@gmail.com>
2020-08-07 10:38:33 -0400

    Adding frontend

---
+++ karaf-system/system.profile/src/main/resources/system.profile/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="flex">
+    <li class="mr-6">
+      <a class="text-blue-500 hover:text-blue-800" href="#">Active</a>
+    </li>
+    <li class="mr-6">
+      <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+    </li>
+    <li class="mr-6">
+      <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+    </li>
+    <li class="mr-6">
+      <a class="text-gray-400 cursor-not-allowed" href="#">Disabled</a>
+    </li>
+  </ul>
+</template>
---
+++ karaf-system/system.profile/src/main/resources/system.profile/src/views/Contact.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="contact">
+    <p>Contact Page</p>
+  </div>
+</template>
