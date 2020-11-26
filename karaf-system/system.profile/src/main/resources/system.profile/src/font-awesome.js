4c6859b580e870b85e47e8e1c4a85002baf4ed70
Patrique Legault <patrique.legault@perficient.com>
2020-11-26 10:53:20 -0500

    Deleting website

--- karaf-system/system.profile/src/main/resources/system.profile/src/font-awesome.js
+++
@@ -1,8 +0,0 @@
-// Font Awesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faGithub, faLinkedin, faTwitter, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
-
-library.add(faGithub)
-library.add(faLinkedin)
-library.add(faTwitter)
-library.add(faStackOverflow)
