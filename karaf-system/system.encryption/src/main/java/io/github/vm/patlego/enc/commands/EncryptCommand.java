f771e04798a58cecc920b96cb9f6ccd504f1bd6a
Patrique Legault <patrique.legault@perficient.com>
2020-12-21 14:02:25 -0500

    Adding missing parameter

--- karaf-system/system.encryption/src/main/java/io/github/vm/patlego/enc/commands/EncryptCommand.java
+++ karaf-system/system.encryption/src/main/java/io/github/vm/patlego/enc/commands/EncryptCommand.java
@@ -1,17 +1,23 @@
 package io.github.vm.patlego.enc.commands;
 
 import org.apache.commons.lang3.StringUtils;
+
 import org.apache.karaf.shell.api.action.Action;
 import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.github.vm.patlego.enc.Security;
 
 @Service
 @Command(scope = "patlego", name = "encrypt", description = "Encrypt a string using Security service")
 public class EncryptCommand implements Action {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Reference
     private Security security;
@@ -21,6 +27,7 @@ public class EncryptCommand implements Action {
 
     @Override
     public Object execute() throws Exception {
+        logger.info("About to encrypt input string");
         return security.encrypt(input);
     }
 
