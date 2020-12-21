ecc5ca30f2a87695e1fd2960b954a4d0e50c97df
Patrique Legault <patrique.legault@perficient.com>
2020-12-21 13:24:06 -0500

    Adding encrypt command class

--- karaf-system/system.encryption/src/main/java/io/github/vm/patlego/enc/commands/EncryptCommand.java
+++ karaf-system/system.encryption/src/main/java/io/github/vm/patlego/enc/commands/EncryptCommand.java
@@ -2,8 +2,8 @@ package io.github.vm.patlego.enc.commands;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
-import org.apache.karaf.shell.api.action.Option;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 
@@ -11,12 +11,12 @@ import io.github.vm.patlego.enc.Security;
 
 @Service
 @Command(scope = "patlego", name = "encrypt", description = "Encrypt a string using Security service")
-public class Encrypt implements Action {
+public class EncryptCommand implements Action {
 
     @Reference
     private Security security;
 
-    @Option(name = "-i", aliases = "--input", description = "Input string to be encrypted", required = true, multiValued = false)
+    @Argument(index = 0, name = "input", description = "Input string to be encrypted", required = true, multiValued = false)
     String input = StringUtils.EMPTY;
 
     @Override
