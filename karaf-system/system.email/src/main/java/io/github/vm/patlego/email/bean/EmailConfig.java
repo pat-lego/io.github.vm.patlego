73efa77d742e5fe44f44eef0f50f159cc3acd54e
Patrique Legault <patrique.legault@perficient.com>
2020-12-21 20:11:38 -0500

    removing logger

--- karaf-system/system.email/src/main/java/io/github/vm/patlego/email/bean/EmailConfig.java
+++ karaf-system/system.email/src/main/java/io/github/vm/patlego/email/bean/EmailConfig.java
@@ -1,11 +1,5 @@
 package io.github.vm.patlego.email.bean;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class EmailConfig {
-
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private String username;
     private String password;
